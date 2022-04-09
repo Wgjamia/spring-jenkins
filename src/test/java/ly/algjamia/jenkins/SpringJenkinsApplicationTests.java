d9cde0e0d1784acf8dda0fa9c7cdfbd3c58bee41
Wesam Algjamia <wgjamia@gmail.com>
2022-04-09 03:15:19 +0200

    first commit

---
+++ src/test/java/ly/algjamia/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package ly.algjamia.jenkins;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+
+	public static Logger logger = (Logger) LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	
+	@Test
+	void contextLoads() {
+		assertEquals(true, true);
+	}
+
+}
